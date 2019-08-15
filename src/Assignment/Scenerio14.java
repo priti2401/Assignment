81b1770c55bf0fdcdc14fbb0dd98876af1cf37b7
Priti <Priti@192.168.245.1>
2019-08-15 13:48:21 +0530

    1s commit

--- src/Assignment/Scenerio14.java
+++ src/Assignment/Scenerio14.java
@@ -8,33 +8,32 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Scenerio14 {
-	
-
-		static
-		{
-			System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
-		}
-          @Test
-		public void a1() throws InterruptedException
-
-		{
-
-			WebDriver driver = new ChromeDriver();// casting
-			driver.manage().window().maximize();// maximize the browser window
-			Actions a = new Actions(driver);
-			driver.navigate().to("https://www.bluestone.com/");
-
-			WebElement ele = driver.findElement(By.xpath("//li[@class='menuparent gold-coins']"));
-			a.moveToElement(ele).perform();
-			Thread.sleep(2000);
-
-			driver.findElement(By.xpath("//span[@data-p='gold-coins-weight-1gms,m']")).click();
-
-			Thread.sleep(2000);
-
-			driver.close();
-		}
+	static
+	{
+		System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
 	}
+     
+	@Test
+	public  static void a1() throws InterruptedException
+
+	{
+
+		WebDriver driver = new ChromeDriver();// casting
+		driver.manage().window().maximize();// maximize the browser window
+		Actions a = new Actions(driver);
+		driver.navigate().to("https://www.bluestone.com/");
+
+		WebElement ele = driver.findElement(By.xpath("//li[@class='menuparent gold-coins']"));
+		a.moveToElement(ele).perform();
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//span[@data-p='gold-coins-weight-1gms,m']")).click();
+
+		Thread.sleep(2000);
+
+		driver.close();
+	}
+}
 
 
 
